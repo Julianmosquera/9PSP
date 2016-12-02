07bb466de6a9c813d7f921fce35e169020151a4c
Julian Mosquera <julianmosqueraguimernas@gmail.com>
2016-12-02 22:07:16 +0100

    formateo

--- ejercicio/pkg9/psp/Multiplos_tres.java
+++ ejercicio/pkg9/psp/Multiplos_tres.java
@@ -1,4 +1,3 @@
-
 package ejercicio.pkg9.psp;
 
 import java.util.logging.Level;
@@ -8,13 +7,12 @@ import java.util.logging.Logger;
  *
  * @author julian
  */
+public class Multiplos_tres extends Thread {
 
+    /**
+     * El primer hilo sumará los múltiplos de 3 del 1 al 1000
+     */
 
-public class Multiplos_tres extends Thread{
-  /**
-   * El primer hilo sumará los múltiplos de 3 del 1 al 1000
-   */
-    
     public int numerovariable;
     public int suma_multiplos_tres;
 
@@ -26,43 +24,27 @@ public class Multiplos_tres extends Thread{
         this.numerovariable = numerovariable;
     }
 
-
-    
-    
-    
-
     public int getSuma_multiplos_tres() {
         return suma_multiplos_tres;
     }
 
-    
-
-   
-      
-    
-     @Override
-   public void run(){
+    @Override
+    public void run() {
         try {
             sleep(5000);
         } catch (InterruptedException ex) {
             Logger.getLogger(Multiplos_tres.class.getName()).log(Level.SEVERE, null, ex);
         }
-       while(suma_multiplos_tres<1000){
-           
-           if(3%numerovariable==0){
-               suma_multiplos_tres=numerovariable+suma_multiplos_tres;
-           
-           }
-          
-           
-      
-       
-       }
-       System.out.println("sumado los multiplos de tres");
-       
-       
-        
-   }
-    
-    
+        while (suma_multiplos_tres < 1000) {
+
+            if (3 % numerovariable == 0) {
+                suma_multiplos_tres = numerovariable + suma_multiplos_tres;
+
+            }
+
+        }
+        System.out.println("sumado los multiplos de tres");
+
+    }
+
 }
